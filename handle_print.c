d4b448c19beeec66e5dede9aceb0ee1f6e3fb248
IamAbasz <135766838+IamAbasz@users.noreply.github.com>
2023-09-21 11:45:22 +0100

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -9,9 +9,10 @@
  * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
+ * This code is been written and edited by Clive and Abasz
  *
  * Return: 1 or 2;
-  * this code is been written and edited by Clive and Abasz
+
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
